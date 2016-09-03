60fa44c1def160db596a4ea2cdea842b6b0b7e53
moltra <webmaster@tosblog.us>
2016-09-04 06:25:28 +0900

    trying to get for command figured out

--- www/js/statementquiz.js
+++ www/js/statementquiz.js
@@ -3,8 +3,14 @@
 
 
 
-window.onload= function ()
-{
 
-    document.getElementById('statement').innerHTML = "George Washington was the first President of the United States.";
+
+    for (i=0;i < 7; i++){
+    
+        document.getElementById('statement').innerHTML = "George Washington was the first President of the United States. " + i + "<br>" ;
+        }
+
+    for (i = 0; i < 5; i++) {
+    document.getElementById('answer').innerHTML = "The number is " + i + "<br>";
+
 }
